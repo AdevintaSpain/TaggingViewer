a158f9d613938ab9bf5014bd0eaf89134dfc0dcb
Bernat Borrás <bernat.borras@adevinta.com>
2022-11-15 19:41:24 +0100

    Install codestyle

---
+++ config/androidstudio/install-codestyle.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Based on Square's codestyle installer (https://github.com/square/java-code-styles/blob/master/install.sh)
+
+echo "Installing Adevinta IntelliJ configs..."
+
+CONFIGS="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+for i in $HOME/Library/Preferences/IntelliJIdea*  \
+         $HOME/Library/Preferences/IdeaIC*        \
+         $HOME/Library/Preferences/AndroidStudio* \
+         $HOME/Library/Application\ Support/JetBrains/IntelliJIdea* \
+         $HOME/.IntelliJIdea*/config              \
+         $HOME/.IdeaIC*/config                    \
+         $HOME/.AndroidStudio*/config \
+         $HOME/Library/Application\ Support/Google/AndroidStudio* \
+         $HOME/Library/Application\ Support/JetBrains/IdeaIC*
+do
+  if [[ -d $i ]]; then
+    # create codestyle folders
+    mkdir -p $i/codestyles
+    # copy xmls
+    cp -frv "$CONFIGS/codestyle"/* $i/codestyles
+  fi
+done
+
+echo "Done."
+echo ""
+echo "Restart IntelliJ and/or AndroidStudio, go to Preferences, Editor, Code Style, and select 'AdevintaAndroid' as scheme."
