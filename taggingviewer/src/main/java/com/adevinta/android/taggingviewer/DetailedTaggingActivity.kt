ae03722ec3c99a5bfef1b524df7d6bceb6e82785
Bernat Borrás <bernat.borras@adevinta.com>
2023-02-01 09:43:48 +0100

    Show / Hide filter

--- taggingviewer/src/main/java/com/adevinta/android/taggingviewer/DetailedTaggingActivity.kt
+++ taggingviewer/src/main/java/com/adevinta/android/taggingviewer/DetailedTaggingActivity.kt
@@ -3,12 +3,15 @@ package com.adevinta.android.taggingviewer
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.Menu
+import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -18,10 +21,13 @@ import com.adevinta.android.taggingviewer.internal.TrackingDispatcher
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
+import kotlin.reflect.KClass
 
 class DetailedTaggingActivity : AppCompatActivity() {
 
   private val adapter: DetailTaggingAdapter = DetailTaggingAdapter()
+
+  private var itemTypes: List<KClass<out TagEntry>> = emptyList()
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -35,26 +41,42 @@ class DetailedTaggingActivity : AppCompatActivity() {
     TrackingDispatcher.entriesData().observe(
       this,
       Observer { entries ->
-        adapter.entries = entries
+        val filteredEntries = entries
           .sortedByDescending { it.timestamp }
           .filterNot { it is TagEntry.SeparatorEntry }
+        adapter.entries = filteredEntries
+
+        itemTypes = filteredEntries.map { it::class }.distinct()
+        (this as MenuHost).invalidateMenu()
       }
     )
-  }
 
-  override fun onCreateOptionsMenu(menu: Menu): Boolean {
-    menuInflater.inflate(R.menu.menu_detailed_tagging, menu)
-    return true
-  }
 
-  override fun onOptionsItemSelected(item: MenuItem): Boolean {
-    return if (item.itemId == R.id.menu_remove) {
-      TaggingViewer.clearAll()
-      adapter.entries = listOf()
-      true
-    } else {
-      super.onOptionsItemSelected(item)
-    }
+    addMenuProvider(object : MenuProvider {
+      override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+        menuInflater.inflate(R.menu.menu_detailed_tagging, menu)
+      }
+
+      override fun onPrepareMenu(menu: Menu) {
+        super.onPrepareMenu(menu)
+
+        menu.findItem(R.id.menu_filter).isVisible = itemTypes.isNotEmpty()
+      }
+
+      override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+        return when (menuItem.itemId) {
+          R.id.menu_remove -> {
+            TaggingViewer.clearAll()
+            adapter.entries = listOf()
+            true
+          }
+          R.id.menu_filter -> {
+            true
+          }
+          else -> false
+        }
+      }
+    }, this)
   }
 }
 
